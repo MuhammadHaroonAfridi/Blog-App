8c7fab5beb696e46b8ad07c795af91ef6036bb4b
Muhammad Haroon <haroontime437@gmail.com>
2025-06-23 13:33:47 -0700

    add email template

--- middlewares/Email/Email.js
+++ middlewares/Email/Email.js
@@ -1,14 +1,15 @@
 import transporter from "./Email.config.js"
+import { Verification_Email_Template } from "./EmailTemplate.js"
 
 
 const sendVerficationEmail =async(email,verificationcode)=>{
     try {
       await  transporter.sendMail({
-        from: ` "Blog APP" ${email}`,
+        from: ` "Blog APP" 'haroontime437@gmail.com' `,
     to: email,
     subject: "Verfiy Email ✔",
     text: "Verify Email", // plain‑text body
-    html: `<h1>${verificationcode}</h1> `, // HTML body
+    html: Verification_Email_Template.replace("{verificationCode}",verificationcode), // HTML body
       })
       console.log('email send successfullly',email)
     } catch (error) {
